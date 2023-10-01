86425f4630708d7b347ab8036396bc22b2058e28
Bruno Amorim <0brunoamorim0@gmail.com>
2023-10-01 11:22:11 -0300

    update profile 	modified:   assets/js/main.js 	modified:   data/profile.json 	modified:   index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
 
+function updateProfileInfo(profileData){
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
+}
+
 (async () => {
     const profileData = await fetchProfileData()
-    console.log(profileData);
+    updateProfileInfo(profileData)
 })()
