68c317122d78098b8b8d1bd19da140498f22ade6
Bruno Amorim <0brunoamorim0@gmail.com>
2023-10-01 13:29:54 -0300

    inicio portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,7 +35,7 @@ function updateHardSkills(profileData){
 
 function updateLanguages(profileData){
     const languages = document.getElementById('profile.languages')
-    languages.innerHTML = profileData.languages.map(language => `<li>${language}</li>`).join('')
+    languages.innerHTML = profileData.languages.map(languages => `<li>${languages}</li>`).join('')
 }
 
 
